2d33fa04f44bfbaf122c5ad7358afe7eedc5f4e4
mecattaf <thomasmecattaf@gmail.com>
2024-05-03 14:10:44 -0400

    drawing inspiration from wayblue

---
+++ config/files/usr/etc/profile.d/settheming.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.desktop.interface gtk-theme 'Catppuccin-Mocha-Standard-Green-Dark'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'ePapirus'
