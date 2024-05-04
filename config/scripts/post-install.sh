16ae42b2be1e17d4aab6ea117a5edfecd392c85b
mecattaf <thomasmecattaf@gmail.com>
2024-05-04 11:35:47 -0400

    post-install.sh attempt 2

--- config/scripts/post-install.sh
+++ config/scripts/post-install.sh
@@ -3,9 +3,6 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-# create new username
-hostnamectl set-hostname dev
-
 # fix to load nvim-treesitter
 ln -s /usr/bin/ld.bfd /usr/local/bin/ld
 
