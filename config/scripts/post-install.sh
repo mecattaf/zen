ade0d805080e9e800d856ff29d652e839e75a782
mecattaf <thomasmecattaf@gmail.com>
2024-05-04 11:25:12 -0400

    post-install.sh attempt

--- config/scripts/post-install.sh
+++ config/scripts/post-install.sh
@@ -1,35 +1,13 @@
 #!/usr/bin/env bash
 
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-
 # Tell build process to exit if there are any errors.
-##set -oue pipefail
-
-# set sddm as default display-manager
-# systemctl set-default graphical.target
-
-# tailscale
-#systemctl enable tailscaled.service
+set -oue pipefail
 
 # create new username
-# hostnamectl set-hostname dev
-
-# ublue flatpak manager (?)
-# note where bluefin saves the flatpaks: https://github.com/ublue-os/bluefin/tree/68b658fa06c22e9ab9a84615c52ae406b1dd021b/usr/etc/flatpak
-# has system and user folders
-#systemctl --global enable ublue-user-flatpak-manager.service 
-
-# flatpak overrides
-#flatpak --user override --filesystem=~/.themes
-#flatpak --user override --filesystem=~/.icons
-##flatpak --user override --env=GTK_THEME=Catppuccin-Mocha-Standard-Green-Dark
+hostnamectl set-hostname dev
 
 # fix to load nvim-treesitter
-##sudo ln -s /usr/bin/ld.bfd /usr/local/bin/ld
+ln -s /usr/bin/ld.bfd /usr/local/bin/ld
 
 # emoji picker
-##pip install emoji-fzf
-
-
+pip install emoji-fzf
