71c123f6661e515ca38ef2a55cd744744cbdd866
mecattaf <thomasmecattaf@gmail.com>
2024-07-11 07:50:41 +0200

    attempting to replace fsync kernel with rpm-ostree override replace kernel

--- config/scripts/post-install.sh
+++ config/scripts/post-install.sh
@@ -3,5 +3,15 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-# fix to load nvim-treesitter
+# The following might have to be done manually upon first boot instead, in which case add to readme:
+
+# 1) fix to load nvim-treesitter
 #ln -s /usr/bin/ld.bfd /usr/local/bin/ld
+
+# 2) enable or load quadlets as systemd service
+#    also note useful flags -xeu in systemctl commands
+#    and journalctl --user
+# systemctl --user daemon-reload
+# systemctl --user start {container_name}.service 
+# systemctl --user list-units # --type=service
+
