b36c63e582c46375ff9e98fee5b0b95c04f1c039
mecattaf <thomasmecattaf@gmail.com>
2024-07-11 06:41:36 +0200

    attempting to replace fsync kernel by first removing kernel

--- config/scripts/fsync-kernel-override.sh
+++ config/scripts/fsync-kernel-override.sh
@@ -3,12 +3,11 @@
 set -oue pipefail
 
 echo 'fsync kernel override'
-#rpm-ostree cliwrap install-to-root / && \
-#rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-cachyos-lto    
 rpm-ostree cliwrap install-to-root
-wget https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-${OS_VERSION}/sentry-kernel-fsync-fedora-${OS_VERSION}.repo -O /etc/yum.repos.d/_copr_kernel-fsync.repo
-rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' \
-kernel \
-kernel-core-* \
-kernel-modules-* \
-kernel-uki-virt-*
+rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install kernel-fsync
+
+#rpm-ostree override replace --experimental --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' \
+#kernel \
+#kernel-core-* \
+#kernel-modules-* \
+#kernel-uki-virt-*
