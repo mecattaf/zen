9009c859da920b698776b9ddc578804e75f47d72
mecattaf <thomasmecattaf@gmail.com>
2024-08-08 10:24:17 +0200

    Using script to install binary directly instead of unstable_cli for rust stage

---
+++ files/scripts/rmpc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+curl -Lo /tmp/rmpc.tar.gz https://github.com/mierak/rmpc/releases/download/v0.2.1/rmpc-v0.2.1-x86_64-unknown-linux-gnu.tar.gz
+mkdir -p /tmp/rmpc
+tar -xvf /tmp/rmpc.tar.gz -C /tmp/rmpc
+cp /tmp/rmpc/rmpc /usr/local/bin
+chmod +x /usr/local/bin/rmpc
