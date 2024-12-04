2d6ab7d9bfb7dc8571beda1ee88360e1c17b2772
mecattaf <thomasmecattaf@gmail.com>
2024-12-04 19:36:58 +0100

    small cleanup

--- files/scripts/rmpc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -oue pipefail
-
-curl -Lo /tmp/rmpc.tar.gz https://github.com/mierak/rmpc/releases/download/v0.2.1/rmpc-v0.2.1-x86_64-unknown-linux-gnu.tar.gz
-mkdir -p /tmp/rmpc
-tar -xvf /tmp/rmpc.tar.gz -C /tmp/rmpc
-cp /tmp/rmpc/rmpc /usr/local/bin
