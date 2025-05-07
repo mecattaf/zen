b070843d254806320e4b9a03bdd11e5a8253abbd
mecattaf <thomasmecattaf@gmail.com>
2025-05-07 19:59:49 +0200

    cleanup: unused fonts, ollama function, old scripts, mouse cursors, and other

--- files/scripts/settheming.sh
+++ files/scripts/settheming.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-sed -i 's/Inherits=Catppuccin-Mocha-Standard-Green-Dark/Inherits=Catppuccin-SE/' /usr/share/icons/default/index.theme 
+sed -i 's/Inherits=Catppuccin-Noir-Standard-Green-Dark/Inherits=Catppuccin-SE/' /usr/share/icons/default/index.theme 
